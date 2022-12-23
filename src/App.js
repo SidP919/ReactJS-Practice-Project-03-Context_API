a3bc3dabc0d070bcce6ae059afc0f93a8488c7d3
sidp919 <sidp0008@gmail.com>
2022-12-23 15:03:57 +0530

    Step-03-Access_Mission_data_using_Consumer_in_App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import Provider from "./Provider";
+import Context from "./Context";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Agent = () => {
+    return (
+        <FindAgent />
+    )
+}
+
+const FindAgent = () => {
+    return (
+        <AgentBond />
+    )
+}
+
+const AgentBond = () => {
+    return(
+        <Context.Consumer>
+            {context => (
+                <Fragment>
+                    <h1>Mission Information:</h1>
+                    <h4>Mission Name: {context.data.mName}</h4>
+                    <h4>Assigned to Agent Code: {context.data.agent}</h4>
+                    <h4>Mission Status: {context.data.accept}</h4>
+                    <button onClick={context.setAccepted}>Accept Mission</button>  
+                </Fragment>
+            )}
+        </Context.Consumer>
+    );
+}
+
+const App = () => {
+    return (
+        <div>
+            <Provider>
+                <Agent />
+            </Provider>
+        </div>
+    )
 }
 
 export default App;
